760343e3a0a8e184dd70433ae2c1fee05a0bb069
Richard Colvin <rxcolvin@yahoo.co.uk>
2015-11-15 13:19:42 +0000

    More changes

--- rxc-meta-data/src/main/java/com/rxc/meta/FieldMeta.java
+++ rxc-meta-data/src/main/java/com/rxc/meta/FieldMeta.java
@@ -3,6 +3,9 @@ package com.rxc.meta;
 /**
  * Created by richard on 15/11/2015.
  */
-public interface FieldMeta {
+public interface FieldMeta<T> {
+    Class<T> tType();
     String name();
+
+    FieldValidator<T> validator();
 }
